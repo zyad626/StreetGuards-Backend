5da0f6af89bb1f3bd6a6da5344fd37d1af6abd47
Zeyad Yasser <zeyad.yasser@travolic.com>
2023-03-03 22:29:01 +0200

    http error handling responses

--- app/Http/Controllers/API/ProductsController.php
+++ app/Http/Controllers/API/ProductsController.php
@@ -13,7 +13,10 @@ class ProductsController extends Controller
     public function create(CreateProductRequest $request)
     {
         $productData = $request->validated();
-
+        if (Product::where('id', $productData["id"])->exists()) {
+            // User with the specified userId already exists
+            return response()->json(["result" => "Product already exists"], 409);
+        }
         $product = new Product;
         $product->fill($productData);
         $product->ip = request()->ip();
@@ -27,17 +30,26 @@ class ProductsController extends Controller
     {
         
         $product = Product::where('id', $id)->get();
+        if($product->isEmpty()){
+            return response()->json(["message" => "Product not found"], 404);
+        }
         return $this->collectionResponse($product, new ProductTransformer);
     }
     public function destroy($id){
-        $product = Product::where('id', $id);
+        $product = Product::where('id', $id)->get();
+        if($product->isEmpty()){
+            return response()->json(["message" => "Product not found"], 404);
+        }
         $product->delete();
         return response()->json(["result" => "ok"], 200);
     }
-    public function update(Request $request,$id){
-        $productData = $request->all();
+    public function update(CreateProductRequest $request,$id){
+        $productData = $request->validated();
 
         $product = Product::where('id', $id)->first();
+        if(!$product){
+            return response()->json(["message" => "Product not found"], 404);
+        }
         $product->fill($productData);
         $product->save();
         
@@ -54,7 +66,9 @@ class ProductsController extends Controller
             $userId = $request->query("content");
 
             $user = User_new::where('userId', $userId)->first();
-            
+            if(!$user){
+                return response()->json(["message" => "User not found"], 404);
+            }
             if($options == "userProducts"){
                 $productIds = $user->products;
                 $Userproducts = Product::whereIn('id', $productIds)->get();
@@ -66,6 +80,7 @@ class ProductsController extends Controller
                 $Userproducts = Product::whereIn('id', $productIds)->get();
                 return $this->collectionResponse($Userproducts, new ProductTransformer);
             }
+            return response()->json(["message" => "Invalid category"], 422);
         }
         
     }
--- app/Http/Controllers/API/UsersController_new.php
+++ app/Http/Controllers/API/UsersController_new.php
@@ -13,30 +13,46 @@ class UsersController_new extends Controller
     {
         $userData = $request->validated();
 
-        $user = new User_new;
-        $user->fill($userData);
-        $user->ip = request()->ip();
-        $user->save();
+        if (User_new::where('userId', $userData["userId"])->exists()) {
+            // User with the specified userId already exists
+            return response()->json(["result" => "User already exists"], 409);
+        }
+        else{
+            $user = new User_new;
+            $user->fill($userData);
+            $user->ip = request()->ip();
+            $user->save();
+    
+            // return $this->itemResponse($user, new UserTransformer);
+            return response()->json(["result" => "ok"], 200);
+        }
 
-        // return $this->itemResponse($user, new UserTransformer);
-        return response()->json(["result" => "ok"], 200);
 
     }
     public function getUser($id)
     {
         
         $user = User_new::where('userId', $id)->get();
+        if($user->isEmpty()){
+            return response()->json(["message" => "User not found"], 404);
+        }
         return $this->collectionResponse($user, new UserTransformer);
     }
     public function destroy($id){
-        $user = User_new::where('userId', $id);
+        $user = User_new::where('userId', $id)->get();
+        if($user->isEmpty()){
+            return response()->json(["message" => "User not found"], 404);
+        }
         $user->delete();
         return response()->json(["result" => "ok"], 200);
     }
-    public function update(Request $request,$id){
+    public function update(CreateUserRequest $request,$id){
 
-        $userData = $request->all();
+        $userData = $request->validated();
         $user = User_new::where('userId', $id)->first();
+        if(!$user){
+            return response()->json(["message" => "User not found"], 404);
+        }
         $user->fill($userData);
         $user->save();
         
