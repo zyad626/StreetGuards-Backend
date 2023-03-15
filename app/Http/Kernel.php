cf0ab58e0e1c24dbb737b8c13afbf65bc25b2672
Zeyad Yasser <zeyad.yasser@travolic.com>
2023-03-15 03:07:54 +0200

    implmented authentication this time in a detecated branch

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -64,6 +64,8 @@ class Kernel extends HttpKernel
         'signed' => \Illuminate\Routing\Middleware\ValidateSignature::class,
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
         'verified' => \Illuminate\Auth\Middleware\EnsureEmailIsVerified::class,
+        'sanctum' => \Laravel\Sanctum\Http\Middleware\EnsureFrontendRequestsAreStateful::class,
+
     ];
 
     /**
